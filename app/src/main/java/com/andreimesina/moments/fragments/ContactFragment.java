69acd3d7eeee9803cf88a73de2301017c836aa66
Andrei Mesina <andrei.mesina@my.fmi.unibuc.ro>
2019-03-29 06:44:28 +0200

    #12520779 Re-design contact us screen

--- app/src/main/java/com/andreimesina/moments/fragments/ContactFragment.java
+++ app/src/main/java/com/andreimesina/moments/fragments/ContactFragment.java
@@ -1,5 +1,7 @@
 package com.andreimesina.moments.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.andreimesina.moments.R;
 
@@ -21,8 +24,30 @@ public class ContactFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setMailListener(view);
+    }
+
+    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         getActivity().setTitle(R.string.contact_title);
+    }
+
+    private void setMailListener(@Nullable View container) {
+        TextView emailView = container.findViewById(R.id.text_developer_email);
+
+        emailView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                        "mailto","andreimesina98@gmail.com", null));
+                mailIntent.putExtra(Intent.EXTRA_SUBJECT, "Mail from Moments");
+
+                getContext().startActivity(Intent.createChooser(mailIntent, "Send mail"));
+            }
+        });
     }
 }
